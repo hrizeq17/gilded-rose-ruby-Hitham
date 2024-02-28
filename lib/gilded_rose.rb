d48e3e8f1be3505261d02308d52d7e59571356de
hrizeq17 <hrizeq@memphis.edu>
2024-02-28 15:09:28 -0600

    fourth task complete

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -30,6 +30,15 @@ class GildedRose
 
   end 
 
+  def backstage_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining <0
+    @quality += 1
+    @quality += 1 if @days_remaining <10
+    @quality += 1 if @days_remaining <5
+  end
+
   
   def tick
     case name
