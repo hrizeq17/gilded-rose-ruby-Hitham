7fd06f92b7d141b6a6b677031af4de3d2b85994e
hrizeq17 <hrizeq@memphis.edu>
2024-02-28 15:07:23 -0600

    third task complete

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,6 +21,15 @@ class GildedRose
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
   end 
+
+
+
+  def sulfuras_tick
+
+
+
+  end 
+
   
   def tick
     case name
@@ -28,6 +37,8 @@ class GildedRose
       return normal_tick
     when 'Aged Brie'
       return brie_tick
+      when 'Sulfuras, Hand of Ragnaros'
+        return sulfuras_tick
     end
    
    
